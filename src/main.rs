1e35974acc3d62815db951214ae6062395f4b558
Thomas Conrad <thomas.conrad@tmrw.com>
2024-08-07 15:37:48 +0200

    Savepoint before leptos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use actix_web::{
+    get,
+    http::StatusCode,
+    web::{self, Data},
+    App, HttpServer, Responder,
+};
 use chrono::DateTime;
 use clap::Parser;
 use futures::stream::{FuturesUnordered, StreamExt};
 use indicatif::ProgressBar;
+use itertools::Itertools;
+use leptos::*;
 use openapi::{
     apis::{
         assets_api::{delete_assets, update_assets},
@@ -14,14 +22,16 @@ use openapi::{
         TimeBucketSize,
     },
 };
-// use serde::{Deserialize, Serialize};
-use itertools::Itertools;
-use std::collections::{BTreeMap, HashMap};
-use std::fs::{self, File};
-use std::io::{self, BufReader, BufWriter};
-use std::time::Duration;
+use serde::Serialize;
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::Arc,
+};
 use thiserror::Error;
+use tokio::sync::RwLock;
 use tracing::info;
+use utoipa::{OpenApi, ToSchema};
+use utoipa_redoc::{Redoc, Servable};
 use uuid::Uuid;
 
 #[derive(Parser, Clone)]
@@ -53,6 +63,8 @@ impl From<Opt> for Configuration {
 
 struct Client {
     configuration: Configuration,
+    assets: Option<HashMap<Uuid, AssetResponseDto>>,
+    count: Option<usize>,
 }
 
 #[derive(Debug, Error)]
@@ -65,8 +77,6 @@ enum ClientError {
     DateTimeParse(#[from] chrono::ParseError),
     #[error("error joining task: {0}")]
     Join(#[from] tokio::task::JoinError),
-    #[error("error io: {0}")]
-    Io(#[from] io::Error),
 }
 
 impl<T> From<Error<T>> for ClientError
@@ -80,7 +90,11 @@ where
 
 impl Client {
     fn new(configuration: Configuration) -> Self {
-        Self { configuration }
+        Self {
+            configuration,
+            assets: None,
+            count: None,
+        }
     }
 
     async fn get_buckets(&self) -> Result<Vec<TimeBucketResponseDto>, ClientError> {
@@ -108,6 +122,13 @@ impl Client {
             .into_iter()
             .map(|b| b.count as usize)
             .sum())
+    }
+
+    async fn get_count(&mut self) -> Result<usize, ClientError> {
+        if self.count.is_none() {
+            self.count = Some(self.count_photos().await?);
+        }
+        Ok(self.count.unwrap())
     }
 
     async fn get_bucket(
@@ -154,102 +175,121 @@ impl Client {
         )
         .await?)
     }
-}
 
-const CACHE_FILE: &str = "assets_cache.json";
-const CACHE_DURATION: Duration = Duration::from_secs(600); // 10 minutes
+    async fn set_assets(&mut self) -> Result<(), ClientError> {
+        if self.assets.is_none() {
+            info!("Fetching assets");
+            let buckets = self.get_buckets().await?;
+            let bar = ProgressBar::new(self.get_count().await? as u64);
+            let mut assets = HashMap::with_capacity(self.get_count().await?);
+            let mut tasks = FuturesUnordered::new();
 
-async fn load_assets_from_cache() -> io::Result<HashMap<Uuid, AssetResponseDto>> {
-    let file = File::open(CACHE_FILE)?;
-    let reader = BufReader::new(file);
-    let assets: HashMap<Uuid, AssetResponseDto> = serde_json::from_reader(reader)?;
-    Ok(assets)
-}
+            for bucket in buckets {
+                let task = self.get_bucket(bucket);
+                tasks.push(task);
+            }
 
-async fn save_assets_to_cache(assets: &HashMap<Uuid, AssetResponseDto>) -> io::Result<()> {
-    let file = File::create(CACHE_FILE)?;
-    let writer = BufWriter::new(file);
-    serde_json::to_writer(writer, assets)?;
-    Ok(())
-}
+            while let Some(result) = tasks.next().await.transpose().unwrap_or(None) {
+                bar.inc(result.len() as u64);
+                for asset in result {
+                    let id = Uuid::parse_str(&asset.id)?;
+                    assets.insert(id, asset);
+                }
+            }
 
-fn is_cache_valid() -> bool {
-    if let Ok(metadata) = fs::metadata(CACHE_FILE) {
-        if let Ok(modified) = metadata.modified() {
-            if let Ok(elapsed) = modified.elapsed() {
-                return elapsed < CACHE_DURATION;
+            drop(tasks);
+
+            bar.finish();
+            self.assets = Some(assets);
+        } else {
+            info!("Assets already fetched");
+        }
+        info!(
+            "Assets fetched, count: {}",
+            self.assets.as_ref().unwrap().len()
+        );
+        Ok(())
+    }
+
+    async fn generate_groups(&mut self) -> Result<Vec<Vec<Uuid>>, ClientError> {
+        self.set_assets().await?;
+
+        let mut assets_tree: BTreeMap<_, Vec<&Uuid>> = BTreeMap::new();
+        for (id, asset) in self.assets.as_ref().unwrap().iter() {
+            let date = DateTime::parse_from_rfc3339(&asset.file_created_at)?;
+            assets_tree.entry(date).or_default().push(id);
+        }
+
+        let mut groups = Vec::<Vec<Uuid>>::new();
+        // Check for assets with the same timestamp
+        let mut group = Vec::new();
+        let mut ongoing = false;
+        for (this, next) in assets_tree.iter().tuple_windows() {
+            if next.0.timestamp_millis() - this.0.timestamp_millis() <= 1000 {
+                if !ongoing {
+                    group.extend(this.1.iter().cloned());
+                    ongoing = true;
+                }
+                group.extend(next.1.iter().cloned());
+            } else if ongoing {
+                group.extend(this.1.iter().cloned());
+                if group.len() > 1 {
+                    groups.push(group)
+                };
+                group = Vec::new();
+                ongoing = false;
             }
         }
+        if !group.is_empty() && group.len() > 1 {
+            groups.push(group);
+        }
+
+        Ok(groups)
     }
-    false
 }
 
-#[tokio::main]
-async fn main() -> Result<(), ClientError> {
+impl actix_web::error::ResponseError for ClientError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
+
+#[derive(Serialize, ToSchema)]
+struct GroupResponse {
+    groups: Vec<Vec<Uuid>>,
+}
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Groups of assets", body = GroupResponse),
+    )
+)]
+#[get("/groups")]
+async fn get_groups(client: web::Data<RwLock<Client>>) -> Result<impl Responder, ClientError> {
+    // Generate groups
+    let groups = client.write().await.generate_groups().await?;
+
+    // Return groups as JSON
+    Ok(web::Json(GroupResponse { groups }))
+}
+
+#[derive(OpenApi)]
+#[openapi(paths(get_groups), components(schemas(GroupResponse)))]
+struct ApiDoc;
+
+fn main()  {
     tracing_subscriber::fmt::init();
+    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    // let openapi = ApiDoc::openapi();
+    // let client = Data::new(RwLock::new(Client::new(Opt::parse().into())));
+    // HttpServer::new(move || {
+    //     App::new()
+    //         .app_data(client.clone())
+    //         .service(get_groups)
+    //         .service(Redoc::with_url("/redoc", openapi.clone()))
+    // })
+    // .bind("127.0.0.1:8080")?
+    // .run()
+    // .await;
 
-    let opt = Opt::parse();
-
-    let configuration = Configuration::from(opt.clone());
-
-    let client = Client::new(configuration.clone());
-    let count = client.count_photos().await?;
-    info!("Library contatins {} photos.", count);
-
-    let assets = if is_cache_valid() {
-        load_assets_from_cache().await?
-    } else {
-        let mut assets = HashMap::with_capacity(count);
-        let buckets = client.get_buckets().await?;
-        let bar = ProgressBar::new(count as u64);
-        let mut tasks = FuturesUnordered::new();
-
-        for bucket in buckets {
-            let task = client.get_bucket(bucket);
-            tasks.push(task);
-        }
-
-        while let Some(result) = tasks.next().await.transpose().unwrap_or(None) {
-            bar.inc(result.len() as u64);
-            for asset in result {
-                let id = Uuid::parse_str(&asset.id)?;
-                assets.insert(id, asset);
-            }
-        }
-
-        save_assets_to_cache(&assets).await?;
-        bar.finish();
-        assets
-    };
-
-    // build binary tree
-    let mut assets_tree: BTreeMap<_, Vec<&Uuid>> = BTreeMap::new();
-    for (id, asset) in assets.iter() {
-        let date = DateTime::parse_from_rfc3339(&asset.file_created_at)?;
-        assets_tree.entry(date).or_default().push(id);
-    }
-
-    let mut groups = Vec::<Vec<&Uuid>>::new();
-    // check for assets with same timestamp
-    let mut group = Vec::new();
-    let mut ongoing = false;
-    for (this, next) in assets_tree.iter().tuple_windows() {
-        if next.0.timestamp_millis() - this.0.timestamp_millis() <= 1000 {
-            if !ongoing {
-                group.extend(this.1);
-                ongoing = true;
-            }
-            group.extend(next.1);
-        } else if ongoing {
-            group.extend(this.1);
-            if group.len() > 1 {
-                groups.push(group)
-            };
-            group = Vec::new();
-            ongoing = false;
-        }
-    }
-
-
-    Ok(())
 }
